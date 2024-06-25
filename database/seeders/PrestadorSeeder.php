1dd32e978537e58bc2e3c59c73765374b9f19bd8
Luciana Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-06-25 12:04:02 -0300

    Update PrestadorSeeder.php

--- database/seeders/PrestadorSeeder.php
+++ database/seeders/PrestadorSeeder.php
@@ -1,13 +1,19 @@
 <?php
+// Início do arquivo PHP e definição do namespace
+
 namespace Database\Seeders;
+// Importação de dependências
 
 use Illuminate\Database\Seeder;
 use App\Models\Prestador;
+// Definição da classe PrestadorSeeder
 
 class PrestadorSeeder extends Seeder
 {
+    // Método run que é executado quando o seeder é chamado
     public function run()
     {
+        // Cria e insere 25 instâncias de Prestador no banco de dados
         Prestador::factory()->count(25)->create();
     }
 }
