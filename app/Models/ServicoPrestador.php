1c00f7f6c6a6a42922b6f08bb55dfc1304c8d634
Luciana Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-06-25 15:02:48 -0300

    Update ServicoPrestador.php

--- app/Models/ServicoPrestador.php
+++ app/Models/ServicoPrestador.php
@@ -4,13 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class ServicoPrestador extends Model
 {
     use HasFactory;
 
     protected $fillable = [
-        'prestador_id', 'servico_id', 'km_saida', 'valor_saida', 'valor_km_excedente'
+        'prestador_id',
+        'servico_id',
+        'km_saida',
+        'valor_saida',
+        'valor_km_excedente'
     ];
 
     public function prestador()
