d813c20fd7d4df5d5ff4f433c00c665585e708d5
Luciana Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-06-25 12:08:49 -0300

    Update Prestador.php

--- app/Models/Prestador.php
+++ app/Models/Prestador.php
@@ -1,19 +1,25 @@
 <?php
+// Início do arquivo PHP
 
-namespace App\Models;
+namespace App\Models; // Define o namespace do modelo
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory; // Importa o trait HasFactory para a classe
+use Illuminate\Database\Eloquent\Model; // Importa a classe base Model do Eloquent
 
+// Declaração da classe Prestador, que estende a classe Model do Eloquent
 class Prestador extends Model
 {
-    use HasFactory;
+    use HasFactory; // Utiliza o trait HasFactory para permitir a geração de fábricas para o modelo
+
+    // Define os atributos que podem ser atribuídos em massa
     protected $fillable = [
         'nome', 'logradouro', 'bairro', 'numero', 'latitude', 'longitude', 'cidade', 'UF', 'situacao'
     ];
 
+    // Método servicos que define o relacionamento N:N (muitos para muitos) com o modelo Servico
     public function servicos()
     {
+        // Retorna o relacionamento, especificando a tabela pivot 'servico_prestadors' e as colunas adicionais na pivot
         return $this->belongsToMany(Servico::class, 'servico_prestadors')
             ->withPivot('km_saida', 'valor_saida', 'valor_km_excedente');
     }
