a139ab431bd5a39403b8ec033775a8006710fc0f
Luciana Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-06-25 12:02:05 -0300

    blocos  comentados

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,16 +1,21 @@
 <?php
+// Define o namespace do arquivo, indicando sua localização dentro da estrutura do projeto
 
 namespace Database\Seeders;
+// Importa as classes necessárias de outros arquivos
 
 use Illuminate\Database\Seeder;
 use App\Models\Prestador;
 use App\Models\Servico;
 use App\Models\ServicoPrestador;
+// Declaração da classe DatabaseSeeder que estende a classe Seeder do Laravel
 
 class DatabaseSeeder extends Seeder
 {
+    // Método run que será chamado para executar os seeders
     public function run()
     {
+        // Chama outros seeders específicos para preencher o banco de dados
         $this->call([
             PrestadorSeeder::class,
             ServicoSeeder::class,
