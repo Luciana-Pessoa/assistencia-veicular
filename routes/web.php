a424b73dc9d7f689fac6b03e2ff46bd5b7f33137
Luciana-Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-06-24 23:35:38 -0300

    rota comentada

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+// Define uma rota GET para a raiz do site. Quando acessada, retorna a view 'welcome'.
 Route::get('/', function () {
     return view('welcome');
 });
