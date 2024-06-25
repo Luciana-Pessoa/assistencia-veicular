0b5c594d7782d6c9639f1053f31195fa5dd00f59
Luciana-Pessoa <103122284+Luciana-Pessoa@users.noreply.github.com>
2024-06-24 23:34:24 -0300

    linhas comentadas

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,16 @@
 <?php
 
+// Define uma rota POST para o login, usando o AuthController
 Route::post('login', [AuthController::class, 'login']);
 
-
+// Define uma rota GET para 'servicos', acessível apenas com autenticação, usando o ServicoController
 Route::middleware('auth:api')->get('servicos', [ServicoController::class, 'index']);
 
-
+// Define uma rota GET para 'geocode/{endereco}', acessível apenas com autenticação, usando o GeocodeController
 Route::middleware('auth:api')->get('geocode/{endereco}', [GeocodeController::class, 'show']);
 
+// Define uma rota POST para 'prestadores', acessível apenas com autenticação, usando o PrestadorController
 Route::middleware('auth:api')->post('prestadores', [PrestadorController::class, 'search']);
 
+// Define uma rota POST para '/prestadors', usando o PrestadorController para criar um novo prestador
 Route::post('/prestadors', [App\Http\Controllers\PrestadorController::class, 'store']);
--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,13 @@
 <?php
 
+// Importa a classe Inspiring da fundação Illuminate
 use Illuminate\Foundation\Inspiring;
+// Importa a fachada Artisan do Illuminate\Support\Facades
 use Illuminate\Support\Facades\Artisan;
 
+// Define um novo comando Artisan chamado 'inspire'
 Artisan::command('inspire', function () {
+    // Dentro da função anônima, usa o método comment para exibir uma citação inspiradora
     $this->comment(Inspiring::quote());
+// Define o propósito do comando como 'Display an inspiring quote' e configura para ser executado a cada hora
 })->purpose('Display an inspiring quote')->hourly();
